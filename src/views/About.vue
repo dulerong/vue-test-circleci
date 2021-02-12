300deb46b007add213d4e58789d9772ea3416765
Le-Jung Du <lejung@qualias.jp>
2021-02-12 18:46:42 +0900

    udpated

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,5 +2,6 @@
   <div class="about">
     <h1>This is Codey's first successful deployment to Heroku with CircleCI!</h1>
     <h2>Life is good 18:49</h2>
+    <h3>Asagaya</h3>
   </div>
 </template>
